c22a31016fd5b30fc31c9d28c721221d4469ec5d
Adityanarayan Tiwari <adityant17121997@gmail.com>
2023-09-13 18:50:16 +0530

    footer color updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import './footer.css';
 const Footer = () => {
   return (
     <div className='footer-section border-top p-2 pt-4 d-flex justify-content-between flex-column flex-lg-row text-center gap-2 pb-5'>
-      <div className=''>
+      <div className='text-secondary'>
         Designed and Coded by{' '}
         <span className='text-purple fw-bold'>Adityanarayan Tiwari</span>
       </div>
-      <div className='text-muted'>© 2023 copyright all right reserved</div>
+      <div className='text-secondary'>© 2023 copyright all right reserved</div>
     </div>
   );
 };
